ae39f73c1985160dec2b5f099d1d32a6b9045261
Liyuanstudy <113154874+Liyuanstudy@users.noreply.github.com>
2022-09-15 17:39:24 +0800

    Add files via upload
    学号：20211424114 姓名：李源

---
+++ 5.10.py
@@ -0,0 +1,23 @@
+total = 0
+average = 0
+count = 0
+
+input_ num = []
+
+while True:
+        num = input('Enter a number:')
+        if num == 'done':
+             break
+        else:
+             try:
+                  num = float(num)
+               input_num. append(num)
+        except:
+               print("Invalid input!")
+
+for itervar in input_num:
+      total = total + itervar
+      count = count +1
+average = total/count
+
+print(total, count , average)
