59d1dfd40d968e9034e5c57c4f92e397674d19b3
Liyuanstudy <113154874+Liyuanstudy@users.noreply.github.com>
2022-09-15 17:53:11 +0800

    Add files via upload
    学号：20211424114    姓名：李源     第一次作业 画图

---
+++ hua tu.py
@@ -0,0 +1,20 @@
+import turtle
+from turtle import
+i=0
+a=3
+pendown()
+while i<3:
+          forward(90)
+          left(120)
+          i=i+1
+left(60)
+forward(30)
+left(60)
+forward(30)
+right(120)
+forward(90)
+right(120)
+forward(90)
+right(120)
+foward(60)
+done()
